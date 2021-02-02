484700495f29ef18d5b8525100ef95d44b9fc19f
sourabhbhandari <bhandari.sourabh372@gmail.com>
2021-02-02 23:04:28 +0530

    Adding GuestBook App

---
+++ guestbook.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+	return render_template('index.html')
+
+@app.route('/sign')
+
+def sign():
+	return render_template('sign.html')
+
+if __name__ == '__main__':
+
+	app.run(debug=True)
+
